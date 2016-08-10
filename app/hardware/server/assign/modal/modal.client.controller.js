410419abc653102a6f90b530de50002e8b4b4661
Zane Hooper <zanehoop@gmail.com>
2016-08-09 23:04:12 -0400

    Create Client in Assign Client Modal
    Closes #188

--- app/hardware/server/assign/modal/modal.client.controller.js
+++ app/hardware/server/assign/modal/modal.client.controller.js
@@ -1,6 +1,11 @@
 (function () {
   'use strict';
 
+  var MODE = {
+    ADD: 0,
+    SEARCH: 1,
+  };
+
   angular
     .module('app.hardware.server')
     .controller('ServerAssignClientModalCtrl', ServerAssignClientModalCtrl)
@@ -11,8 +16,17 @@
    *
    * @ngInject
    */
-  function ServerAssignClientModalCtrl(Select, servers, access) {
+  function ServerAssignClientModalCtrl(
+    Select,
+    $translateModuleLoader,
+    $translate,
+    Api,
+    _,
+    servers,
+    access
+  ) {
     var modal = this;
+    var $client = Api.all('client');
 
     modal.access = _.assign({
       client: null,
@@ -24,15 +38,52 @@
     modal.servers = servers;
     modal.client = Select('client');
     modal.submit = submit;
+    modal.mode = _.assign({
+      value: MODE.SEARCH,
+    }, MODE);
+    modal.create = {};
 
     activate();
 
     //////////
 
     function activate() {
+      $translateModuleLoader.addPart('user');
+      $translate.refresh();
     }
 
     function submit() {
+      switch(modal.mode.value) {
+      case MODE.ADD:
+        return createClient()
+          .catch(function (response) {
+            if ((response.data.data || {}).id) {
+              return response.data.data;
+            }
+          })
+          .then(function (client) {
+            if (!client) {
+              throw new Error('Failed to create client');
+            }
+
+            modal.access.client = client;
+
+            return client;
+          })
+          .then(sendResult)
+          ;
+      case MODE.SEARCH:
+        return sendResult();
+      }
+    }
+
+    function createClient() {
+      return $client
+        .post(modal.create.getData())
+        ;
+    }
+
+    function sendResult() {
       return modal.$close(!modal.access.client ? null : {
         client: {
           id: modal.access.client.id,

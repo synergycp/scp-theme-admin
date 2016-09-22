08f0bd2edcf35614f7173d6b7e33be90ffed209b
Zane Hooper <zanehoop@gmail.com>
2016-09-22 01:38:48 -0400

    IPMI & Switch control confirmations
    Closes #333
    Closes #334

--- app/hardware/server/switch/switch.component.js
+++ app/hardware/server/switch/switch.component.js
@@ -17,7 +17,7 @@
   /**
    * @ngInject
    */
-  function ServerSwitchControlCtrl(Api, Loader, $scope, $state) {
+  function ServerSwitchControlCtrl(Api, Loader, Modal, $scope, $state) {
     var switchControl = this;
 
     $scope.$state = $state;
@@ -29,16 +29,41 @@
     switchControl.patch = patch;
     switchControl.$onInit = init;
 
+    switchControl.power = {
+      on: patch.bind(null, {
+        power: 'on',
+      }),
+      off: confirmPowerOff,
+    };
+
     //////////
 
     function init() {
       switchControl.isEnabled = switchControl.server.is_switch_ready;
     }
 
+    function confirmPowerOff() {
+      var patchData = {
+        power: 'off',
+      };
+
+      return switchControl.loader.during(
+        Modal
+          .confirm([switchControl.server], 'server.switch.power.off.confirm')
+          .data({
+            port: switchControl.server.switch.port,
+            switch: switchControl.server.switch.name,
+          })
+          .open()
+          .result
+          .then(patch.bind(null, patchData))
+      );
+    }
+
     function patch(data) {
       return switchControl.loader.during(
         switchControl.server
-          .one('switch/'+switchControl.server.hub.id)
+          .one('switch/'+switchControl.server.switch.id)
           .patch(data)
           .then(showResponse)
           .then(fireChangeEvent)

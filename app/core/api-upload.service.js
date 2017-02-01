bec52e94d1b275696b9a918e7ea5bf6c97a42ff4
Zane Hooper <zanehoop@gmail.com>
2017-02-01 18:47:49 -0500

    Fix

--- app/core/api-upload.service.js
+++ app/core/api-upload.service.js
@@ -20,10 +20,10 @@
 
     function upload(method, url, file, data) {
       url = Api.baseUrl() + 'api/' + url + '?key=' + ApiKey.get();
-      data._method = method; // Laravel bug handling multipart/form-data w/ PATCH
+      //data._method = method; // Laravel bug handling multipart/form-data w/ PATCH
       file.upload = Upload.upload({
         url: url,
-        method: 'POST',
+        method: method,
         data: data,
         headers: {
         //  'Content-Type': 'application/x-www-form-urlencoded',

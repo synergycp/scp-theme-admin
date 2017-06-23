e843a56d8cf19d5ee3cf57ac6fee3752160c0d07
Zane Hooper <zanehoop@gmail.com>
2017-06-22 20:16:35 -0400

    HTML 5 Mode

--- app/core/core.config.js
+++ app/core/core.config.js
@@ -15,7 +15,7 @@
 
     // Set the following to true to enable the HTML5 Mode
     // You may have to set <base> tag in index and a routing configuration in your server
-    $locationProvider.html5Mode(false);
+    $locationProvider.html5Mode(location.port < 8000);
   }
 
 })();

ffcba15aa5561ad37a1960f1323d00b2208e5756
Андрей Михацкий <andrey.mikhatskiy@gmail.com>
2017-06-25 11:14:05 +0300

    reverted core config changes

--- app/core/core.config.js
+++ app/core/core.config.js
@@ -11,7 +11,7 @@
    */
   function coreConfig($compileProvider, $animateProvider, $locationProvider) {
     // Disable debug info for performance boost.
-    $compileProvider.debugInfoEnabled(true);
+    $compileProvider.debugInfoEnabled(false);
 
     // Set the following to true to enable the HTML5 Mode
     // You may have to set <base> tag in index and a routing configuration in your server

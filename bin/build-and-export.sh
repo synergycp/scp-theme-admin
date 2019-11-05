8132fd71a86ec0753862ab0a5a4305c3d09eaa96
Zane Hooper <zanehoop@gmail.com>
2019-11-05 10:53:09 -0700

    Improve build process

--- bin/build-and-export.sh
+++ bin/build-and-export.sh
@@ -2,5 +2,14 @@
 
 VERSION=$1
 
-gulp prod build
-tar -zcvf "/scp/install.synergycp.com/bm/${VERSION}/theme/default/admin.tar.gz" --transform 's,^public,admin,' public
+exit-with-error() {
+  CODE=$?
+  echo "ERROR: $@" > /dev/stderr
+  exit $CODE
+}
+
+DEST="/scp/install.synergycp.com/bm/${VERSION}/theme/default/admin.tar.gz"
+gulp prod build || exit-with-error "Gulp build failed"
+tar -zcf "$DEST" --transform 's,^public,admin,' public || exit-with-error "Building archive failed"
+echo "Successfully built the latest theme files to:"
+echo "$DEST"

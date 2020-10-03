611f9d18bf3e335378d6989b137aeb835f9f11d6
Zane Hooper <zanehoop@gmail.com>
2020-10-03 15:30:34 -0600

    Ability to convert IP Entities to IP Pools

--- app/network/entity/list/list.factory.js
+++ app/network/entity/list/list.factory.js
@@ -14,8 +14,12 @@
     return function () {
       var list = List('entity');
       list.confirm = ListConfirm(list, 'entity.modal.delete');
+      var convertToPool = list.confirm.deleter('entity.modal.convert', {
+        create_pool: true,
+      });
 
       list.bulk.add('Assign IP Group', wrapChangeEvent(EntityAssign.group));
+      list.bulk.add('Convert to IP Pool', convertToPool);
       list.bulk.add('Delete', list.confirm.delete);
 
       return list;

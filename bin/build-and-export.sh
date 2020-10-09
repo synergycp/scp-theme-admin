0a46b0eb72885a7412b1eeded7a5f83e86405bce
Zane Hooper <zanehoop@gmail.com>
2020-10-09 02:54:33 -0400

    Build and export for pipeline

--- bin/build-and-export.sh
+++ bin/build-and-export.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-VERSION=$1
-
 exit-with-error() {
   CODE=$?
   echo "ERROR: $@" > /dev/stderr
   exit $CODE
 }
 
-DEST="/scp/install.synergycp.com/bm/${VERSION}/theme/default/admin.tar.gz"
-nodejs ./node_modules/.bin/gulp prod build || exit-with-error "Gulp build failed"
-tar -zcf "$DEST" --transform 's,^public,admin,' public || exit-with-error "Building archive failed"
-echo "Successfully built the latest theme files to:"
-echo "$DEST"
+./node_modules/.bin/gulp prod build || exit-with-error "Gulp build failed"
+tar -zcvf "../build/admin.tar.gz" --transform 's,^public,admin,' public || exit-with-error "Archive build failed"

9b5553aebb0b0ed439cf7d84a09949d038a205a2
Zane Hooper <zanehoop@gmail.com>
2022-01-09 16:32:07 +0000

    Change install page title

--- app/pxe/install/install.routes.js
+++ app/pxe/install/install.routes.js
@@ -1,8 +1,5 @@
 (function () {
-  angular
-    .module('app.pxe.install')
-    .config(routeConfig)
-    ;
+  angular.module("app.pxe.install").config(routeConfig);
 
   /**
    * @ngInject
@@ -10,19 +7,18 @@
   function routeConfig($stateProvider, RouteHelpersProvider) {
     var helper = RouteHelpersProvider;
     $stateProvider
-      .state('app.pxe.install', {
-        url: '/install',
+      .state("app.pxe.install", {
+        url: "/install",
         abstract: true,
         template: helper.dummyTemplate,
       })
-      .state('app.pxe.install.list', {
-        url: '?q',
-        title: 'PXE Installs',
-        controller: 'InstallIndexCtrl as vm',
+      .state("app.pxe.install.list", {
+        url: "?q",
+        title: "OS Reloads",
+        controller: "InstallIndexCtrl as vm",
         reloadOnSearch: false,
-        templateUrl: helper.basepath('pxe/install/list/install.index.html'),
-        resolve: helper.resolveFor('lang:os-reload', 'lang:server'),
-      })
-      ;
+        templateUrl: helper.basepath("pxe/install/list/install.index.html"),
+        resolve: helper.resolveFor("lang:os-reload", "lang:server"),
+      });
   }
 })();

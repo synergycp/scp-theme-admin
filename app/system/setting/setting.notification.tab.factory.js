ab390a7d441e98bef2e67654f0fa6490196fe6c0
Daniel Esteves <danielzesteves@gmail.com>
2023-10-01 08:20:32 -0300

    Crud: Create a notification

--- app/system/setting/setting.notification.tab.factory.js
+++ app/system/setting/setting.notification.tab.factory.js
@@ -10,8 +10,12 @@
    *
    * @ngInject
    */
+  const OWNER_TYPE = 'App\\Admin\\Admin';
+  var OWNER = null;
   function NotificationTab(Loader, Api) {
     return function(vm) {
+      const storage = JSON.parse(localStorage.getItem('ngStorage-admin.apiKey'));
+      OWNER = storage.owner;
       var tab = this;
       var $api = Api.one('notification');
 
@@ -19,8 +23,8 @@
       tab.body = 'app/system/setting/setting.notification.html';
       tab.active = false;
       tab.visible = true;
-      console.log("NotificationTab");
       tab.engines = [];
+      tab.getChannels = getChannels;
       tab.form = {
         submit
       };
@@ -41,8 +45,48 @@
       }
 
       function submit() {
-        console.log(tab.form);
+        const requestFunction = `${tab.form.engine.name.toLowerCase()}Request`;
+        const request = eval(requestFunction + "()");
+        Api.all('notification')
+          .post(request)
+          .then(function (response) {
+            console.log(response);
+          })
       }
+
+      function getChannels() {
+      }
+      function slackRequest() {
+        try {
+          const recipients = tab.form.slack.channels.split(',').map((channel_code)=>{
+            return {
+              "channel_code":channel_code
+            };
+          });
+          return {
+            "credentials":{
+              "token": tab.form.slack.token
+            },
+            "owner_id": OWNER.id,
+            "owner_type": OWNER_TYPE,
+            "recipients":recipients,
+            "notification_type":tab.form.engine.id,
+          };
+        } catch (error) {
+          console.log(error);
+          return null
+        }
+      }
+
+      // return [
+      //   "credentials" => ['token' => 'xoxb-5846175915265-5819196402359-DlqU6ThNUpCoKNaGA85vMDKy'],
+      //   "notification_type" => NotificationEngineType::SLACK,
+      //   "recipients" => [
+      //     ['channel_code' => 'C05Q3095PST']
+      //   ],
+      //   "owner_type" => get_class($testAdmin),
+      //   "owner_id" => $testAdmin->id
+      // ];
     }
   }
 })();

7a63d593cf66f8f0b9f3054bdaf3c901b34f26bd
Daniel Esteves <danielzesteves@gmail.com>
2026-05-07 11:31:46 -0300

    [BA-674] Health check for file server

--- app/pxe/file/list/list.factory.js
+++ app/pxe/file/list/list.factory.js
@@ -10,12 +10,19 @@
    *
    * @ngInject
    */
-  function PxeFileListFactory (List, ListConfirm) {
+  function PxeFileListFactory (Api, List, ListConfirm) {
     return function () {
       var list = List('pxe/file');
       list.confirm = ListConfirm(list, 'pxe.file.modal.delete');
 
+      list.bulk.add('Run health check', runHealthCheck);
       list.bulk.add('Delete', list.confirm.delete);
+
+      function runHealthCheck(items) {
+        return Api.all('pxe/file/health-check')
+          .post({ids: _.map(items, 'id')})
+          .then(list.load);
+      }
 
       return list;
     };
